defc4a9667e8351e4f5be72c90649c10d7851890
Nane <daniel2000rincon@gmail.com>
2020-10-10 21:23:36 -0500

    Actividad 5
    Configuración de proyecto, acceso a datos y múltiples rutas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const { pokemon } = require('./pokedex.json');
 
 /*
 Verbos HTTP
@@ -19,11 +19,44 @@ get(url después de ip y puerto,req: petición que hace el cliente,
     res: respuesta que vamos a dar,next)
 */
 app.get("/",(req,res,next)=>{
-    res.status(200)
-    res.send("Hola Mundo");
+    //res.send(pokemon);
+    res.status(200);
+    res.send("Bienvenido al Pokedex");
 })
 
+app.get("/pokemon/all",(req,res,next)=>{
+    //console.log(req.params.name);
+    res.status(200);
+    //res.send("Hola, "+req.params.name);
+    res.send(pokemon);
+})
 
-app.listen(3000,()=>{
+app.get('/pokemon/:id([0-9]{1,3})',(req,res,next)=>{
+    const id = req.params.id -1;
+    if(id >= 0 && id < 151){
+        res.status(200);
+        res.send(pokemon[req.params.id-1]);
+        // o -> return res.send(pokemon[req.params.id-1]); sin el else
+    }else{
+        res.status(404);
+        res.send("Pokémon no encontrado");
+    }
+    
+})
+
+app.get('/pokemon/:name',(req,res,next)=>{
+    const name = req.params.name;
+    for(i=0;i<pokemon.length;i++){
+        if(pokemon[i].name == name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokémon no encontrado");
+
+})
+
+app.listen(process.env.PORT || 3000,()=>{
     console.log("server is running...");
 });
